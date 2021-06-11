cbfb2ae0c60e0963baebe0664617f425268c7e53
kalai <skalaiarasy@gmail.com>
2021-06-10 20:06:19 -0400

    yelp api is working

--- ResturantAGoGo/ClientApp/src/app/restaurantapi.service.ts
+++ ResturantAGoGo/ClientApp/src/app/restaurantapi.service.ts
@@ -1,19 +1,32 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { HttpHeaders } from '@angular/common/http';
-
 @Injectable({ providedIn: 'root' })
 export class RestaurantapiService {
-  apiKey: string = "Bearer Bmtr7SZS4RMwjn9cC6UIKcpekqpc79fHskbIQAJ096rBentfTjyV9OKtN2BjDfEz6dC6DkHcPVlgdjg74jw3tS8JsAvqOXv5folWDnSkN0WfJe6l2DNaySVEXSTBYHYx";
-  apiUrl: string = "https://api.yelp.com/v3/businesses/search?location=Troy,MI";
-  constructor(private http: HttpClient ) {
-
+  apiUrl: string = "/yelp/businesses/search?location=Troy,MI";
+  constructor(private http: HttpClient) {
   }
-  //get the resturants method
-
+  //To get the restaurants details 
   getAllRestaurants(): any {
-    const headers = new HttpHeaders().set('Authorization',this.apiKey);
-    return this.http.get(this.apiUrl, { headers:headers });
-
+    return this.http.get(this.apiUrl);
   }
 }
+////import { Injectable } from '@angular/core';
+////import { HttpClient } from '@angular/common/http';
+////import { HttpHeaders } from '@angular/common/http';
+
+////@Injectable({ providedIn: 'root' })
+////export class RestaurantapiService {
+////  apiKey: string = "Bearer Bmtr7SZS4RMwjn9cC6UIKcpekqpc79fHskbIQAJ096rBentfTjyV9OKtN2BjDfEz6dC6DkHcPVlgdjg74jw3tS8JsAvqOXv5folWDnSkN0WfJe6l2DNaySVEXSTBYHYx";
+////  apiUrl: string = "https://api.yelp.com/v3/businesses/search?location=Troy,MI";
+////  constructor(private http: HttpClient ) {
+
+////  }
+////  //To get the restaurants details 
+
+////  getAllRestaurants(): any {
+////    const headers = new HttpHeaders().set('Authorization',this.apiKey);
+////    return this.http.get(this.apiUrl, { headers:headers });
+
+////  }
+////}
